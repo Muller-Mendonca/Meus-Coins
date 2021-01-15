b2f69fe2a2cc2e008339f0aca57b220752844443
Muller Mendonça <mullerm.oficial@gmail.com>
2021-01-15 09:35:23 -0300

    cotaçao do bitcoin adicionada ao site

--- module/price.js
+++ module/price.js
@@ -2,5 +2,12 @@
 export default function initPrice(){
     console.log('iniciou preco')
     const url = 'https://blockchain.info/ticker';
+    const btcPreco = document.querySelector('[data-js="quotation"]');
+    fetch(url)
+    .then(response => response.json())
+    .then((bitcoin) =>{
+    btcPreco.innerText = (bitcoin.BRL.sell)
+    .toFixed(4);
+      }).catch(erro => console.log(Error(erro)));
     
 };
