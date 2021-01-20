bcc378a7c6ce55bb0ebe4790e684ac30f3c1af57
Muller Mendonça <mullerm.oficial@gmail.com>
2021-01-20 08:47:55 -0300

    falta pegar o valor da cotação e dividir pelo valor do patrimonio acumulado

--- module/price.js
+++ module/price.js
@@ -25,9 +25,9 @@ export default function initPrice(){
   const pValue = document.querySelector("[data-js=p-value]");
   const result = document.querySelector("[data-js=result]");
   function patrimony(){
-   
-    console.log('patrimonio')
-    console.log(result)
+   console.log(pValue.value);
+    
+    result.innerText = ( 2000 / pValue.value);
   }
   pValue.addEventListener('mouseout', patrimony);
 
