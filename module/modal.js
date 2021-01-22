0613b2c3f02095e7eef2ce23c47342cf06885c60
Muller Mendonça <mullerm.oficial@gmail.com>
2021-01-22 11:33:12 -0300

    funcionalidade criada

--- module/modal.js
+++ module/modal.js
@@ -1,5 +1,12 @@
 
 export default function initModal(){
-    const modalBtn = document.querySelector("[]")
+    const modalBtn = document.querySelector("[data-js=btn-modal]")
+    const modalPatrimony = document.querySelector("[ta-js=modal-patrimony]")
 
+    function handleClick(){
+        modalPatrimony.classList.add('modal-active')
+        console.log('clicou')
+    }
+
+    modalBtn.addEventListener('click', handleClick);
 };
