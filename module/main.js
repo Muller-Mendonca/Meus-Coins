171614663fd3696d5762ce6777dbfcc52a7b72b5
Muller Mendonça <mullerm.oficial@gmail.com>
2021-01-22 11:29:14 -0300

    iniciando a funcionalidade do modal com js

--- module/main.js
+++ module/main.js
@@ -1,6 +1,8 @@
 import initMenuMobile from './menuMobile.js'
 import initPrice from './price.js'
+import initModal from './modal.js'
 
 
 initMenuMobile();
 initPrice();
+initModal()
---
+++ module/modal.js
@@ -0,0 +1,5 @@
+
+export default function initModal(){
+    const modalBtn = document.querySelector("[]")
+
+};
