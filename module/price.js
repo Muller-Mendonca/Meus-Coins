073d079f261f3f24e589d33e57bc42e74387ea9e
Muller Mendonça <mullerm.oficial@gmail.com>
2021-02-24 10:01:40 -0300

    retirando comentarios

--- module/price.js
+++ module/price.js
@@ -1,6 +1,5 @@
 
 export default function initPrice(){
- // const update = document.querySelector("[data-js=update-price]");
 
   function startFech(){
     const url = 'https://blockchain.info/ticker';
@@ -14,23 +13,6 @@ export default function initPrice(){
       }).catch(erro => console.log(Error(erro)));
       
   }
-
-   
- // function updatePrice(e){
- //   e.preventDefault();
- //   startFech();
- // };
- // update.addEventListener('click', updatePrice);
-
-
-
-
- // const pValue = document.querySelector("[data-js=p-value]");
- // const result = document.querySelector("[data-js=result]");
- // function patrimony(){
- //   result.innerText = ( 2000 / pValue.value);
- // }
- // pValue.addEventListener('mouseout', patrimony);
 
  return startFech()
 };
