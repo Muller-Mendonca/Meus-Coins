097897ee82ac5bdb28ecdcbbd3b09094293fbdbd
Muller Mendonça <mullerm.oficial@gmail.com>
2021-02-24 10:23:07 -0300

    adicionado a animação ao abrir o modal

--- module/modal.js
+++ module/modal.js
@@ -3,11 +3,14 @@ export default function initModal(){
     const modalBtn = document.querySelector("[data-js=btn-modal]")
     const modalPatrimony = document.querySelector("[data-js=modal-patrimony]")
     const close = document.querySelector("[data-js=close]")
+    const focus = document.querySelector("[data-js=focusInput]")
 
-    function handleClick(){
+    function handleClick(e){
+        e.preventDefault()
         modalPatrimony.classList.add('modal-active')
     }
     modalBtn.addEventListener('click', handleClick);
+    focus.addEventListener('click', handleClick);
 
     function closeModal(){
         modalPatrimony.classList.remove('modal-active')
