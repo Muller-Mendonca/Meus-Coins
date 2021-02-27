e0c348ee7101eb332d2a7971055dbdba3b5840ed
Muller Mendonça <mullerm.oficial@gmail.com>
2021-02-27 08:49:03 -0300

    refatorando price

--- module/price.js
+++ module/price.js
@@ -1,13 +1,26 @@
 
-  export default function startFech(){
+const btcPreco = document.querySelector('[data-js="quotation"]');
+  
+export default async function startFech(){
     const url = 'https://blockchain.info/ticker';
-    const btcPreco = document.querySelector('[data-js="quotation"]');
-    fetch(url)
-    .then(response => response.json())
-    .then((bitcoin) =>{
-      const bit = Number(bitcoin.BRL.sell.toFixed(0));
-       btcPreco.innerText = bit
-      }).catch(erro => console.log(Error(erro)));
+    const response = await fetch(url)
+    const json = await response.json()
+    const bit = Number(json.BRL.sell.toFixed(0))
+    btcPreco.innerText = bit
+    console.log(bit)
   }
 
-  
+
+
+
+//  export default function startFech(){
+//    const url = 'https://blockchain.info/ticker';
+//    const btcPreco = document.querySelector('[data-js="quotation"]');
+//    fetch(url)
+//    .then(response => response.json())
+//    .then((bitcoin) =>{
+//      const bit = Number(bitcoin.BRL.sell.toFixed(0));
+//       
+//      }).catch(erro => console.log(Error(erro)));
+//  }
+//  
